1b550a8fe55255106ad60ca09d28abb55eaa72e0
ZzOneDay <azminseo@gmail.com>
2020-03-25 02:21:34 +0300

    Repair mavin Project, Connect to mySQL by last code. Add last code when connect to mySQL is work. Add connect to mySQL by Notebook this WiFi

---
+++ 10_SQLAndHibernate/src/main/java/Main.java
@@ -0,0 +1,50 @@
+import java.sql.*;
+
+public class Main {
+
+    private static final String url = "jdbc:mysql://192.168.0.102:3306/skillbox?useUnicode=true&serverTimezone=UTC";
+    private static final String user = "root";
+    private static final String pass = "test1234";
+
+
+
+    private static Connection connection;
+    private static Statement statement;
+    private static ResultSet resultSet;
+
+
+    public static void main(String[] args) {
+        String query = "SELECT course_name, " +
+                "DateDIFF(max(subscription_date), min(subscription_date))/(count(student_name)*30) " +
+                "FROM skillbox.purchaselist " +
+                "GROUP BY course_name";
+
+        try {
+            connection = DriverManager.getConnection(url, user, pass);
+
+            statement = connection.createStatement();
+
+            resultSet = statement.executeQuery(query);
+            System.out.println("Connect opened...");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(1) + " | " + resultSet.getString(2));
+            }
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                connection.close();
+                statement.close();
+                resultSet.close();
+                System.out.println("Connect closed.");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
+
