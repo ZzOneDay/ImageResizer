7deef31b32498fab5c61a529b07befa0eabe6516
ZzOneDay <azminseo@gmail.com>
2020-04-03 23:50:18 +0300

    Created some code about Create, Update, Delete object in mySQL by Transaction.

--- 10_SQLAndHibernate/src/main/java/Main.java
+++ 10_SQLAndHibernate/src/main/java/Main.java
@@ -1,7 +1,8 @@
+import org.hibernate.Transaction;
 import table.Course;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import table.PurchaseList;
+import table.CourseType;
 
 
 public class Main {
@@ -13,10 +14,30 @@ public class Main {
         SessionFactory sessionFactory = connectorToMySQL.getSessionFactory();
         Session session = sessionFactory.openSession();
 
-        Course course = session.get(Course.class, 1);
-        System.out.println(course.getName());
-        PurchaseList purchaseList = session.get(PurchaseList.class, 1);
-        System.out.println(purchaseList.getSubscriptionDate());
+        Transaction transaction = session.beginTransaction();
+//        Создаем новый курс.
+//        Course course = getNewCourse("Новый новый курс", CourseType.DESIGN,1);
+//        session.save(course);
+
+//        Редактируем курс.
+//        Course course = session.get(Course.class, 50);
+//        course.setName("Актуальный курс");
+//        session.save(course);
+
+//        Удаляем курс по ID
+//        Course course = session.get(Course.class, 48); //id не по порядку создаются
+//        System.out.println(course.getName());
+//        session.delete(course);
+
+        transaction.commit();
+    }
+
+    private static Course getNewCourse(String name, CourseType courseType, int teacherId) {
+        Course course = new Course();
+        course.setName(name);
+        course.setType(courseType);
+        course.setTeacherId(teacherId);
+        return course;
     }
 }
 
